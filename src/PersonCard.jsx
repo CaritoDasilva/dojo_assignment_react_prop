e0fb64686d785bc75081d62581d4bf96f5cdd4ff
CaritoDasilva <carito@LAPTOP-9RNF9S4H.localdomain>
2020-09-07 01:38:43 -0300

    button age added

--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class PersonCard extends Component {
-    render() {
-        const { firstName, lastName, age, hair } = this.props
+const PersonCard = (props) => {
+    const { firstName, lastName, age, hair } = props
+    const [ageInCOmponent, setAgeInCOmpoennt] = useState(age)
+    
         return (
             <div>
                 <h1>Hi {lastName}, {firstName}</h1>
                 
                 <h5>Hair: {hair}</h5>
-                <h5>Age: {age}</h5>
+                <h5>Age: {ageInCOmponent}</h5>
+                <button onClick={() => setAgeInCOmpoennt(ageInCOmponent + 1)}>Add a year!</button>
 
             </div>
         )
-    }
+    
 }
+
+export default PersonCard
